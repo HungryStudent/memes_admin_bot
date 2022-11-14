be67ffb5edc7c2838b41cbadb986754e9cd67f9f
HungryStudent <nikita.efan.2016@mail.ru>
2022-11-14 21:23:21 +0300

    create

--- cat_post.py
+++ cat_post.py
@@ -1,30 +1,36 @@
+import asyncio
 from contextlib import closing
 import sqlite3
-
-import requests
+from aiogram import Bot
 import random
 import os
 
 from config import *
 
+bot = Bot(token=TOKEN)
 database = "database.db"
-url = f"https://api.telegram.org/bot{TOKEN}"
-with closing(sqlite3.connect(database)) as connection:
-    cursor = connection.cursor()
-    cursor.execute("SELECT id, file_id, file_type FROM content WHERE chat = ? ORDER BY id LIMIT 1",
-                   (chats[cat_chat_id],))
-    cat = cursor.fetchone()
-    if cat is None:
-        reserv_photo = open(os.path.join("reserv/catdelo", random.choice(os.listdir("reserv/catdelo"))), "rb")
-        name_photo = reserv_photo.name
-        files = {"photo": reserv_photo}
-        requests.post(url + "/sendPhoto", data={"chat_id": cat_channel_id}, files=files)
-        reserv_photo.close()
-        os.remove(reserv_photo.name)
-    else:
-        cursor.execute("DELETE FROM content WHERE id = ?", (int(cat[0]),))
-        connection.commit()
-        if cat[2] == "photo":
-            requests.post(url + "/sendPhoto", json={"chat_id": cat_channel_id, "photo": cat[1]})
+
+
+async def main():
+    with closing(sqlite3.connect(database)) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, file_id, file_type FROM content WHERE chat = ? ORDER BY id LIMIT 1",
+                       (chats[cat_chat_id],))
+        cat = cursor.fetchone()
+        if cat is None:
+            reserv_photo = open(os.path.join("reserv/catdelo", random.choice(os.listdir("reserv/catdelo"))), "rb")
+            name_photo = reserv_photo.name
+            await bot.send_photo(memes_channel_id, photo=reserv_photo)
+            reserv_photo.close()
+            os.remove(name_photo)
         else:
-            requests.post(url + "/sendAnimation", json={"chat_id": cat_channel_id, "animation": cat[1]})
+            cursor.execute("DELETE FROM content WHERE id = ?", (int(cat[0]),))
+            connection.commit()
+            if cat[2] == "photo":
+                await bot.send_photo(memes_channel_id, photo=cat[1])
+            else:
+                await bot.send_animation(memes_channel_id, animation=cat[1])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- mem_post.py
+++ mem_post.py
@@ -1,30 +1,36 @@
+import asyncio
 from contextlib import closing
 import sqlite3
-
-import requests
+from aiogram import Bot
 import random
 import os
 
 from config import *
 
+bot = Bot(token=TOKEN)
 database = "database.db"
-url = f"https://api.telegram.org/bot{TOKEN}"
-with closing(sqlite3.connect(database)) as connection:
-    cursor = connection.cursor()
-    cursor.execute("SELECT id, file_id, file_type FROM content WHERE chat = ? ORDER BY id LIMIT 1",
-                   (chats[memes_chat_id],))
-    mem = cursor.fetchone()
-    if mem is None:
-        reserv_photo = open(os.path.join("reserv/memdelo", random.choice(os.listdir("reserv/memdelo"))), "rb")
-        name_photo = reserv_photo.name
-        files = {"photo": reserv_photo}
-        res = requests.post(url + "/sendPhoto", data={"chat_id": memes_channel_id}, files=files)
-        reserv_photo.close()
-        os.remove(name_photo)
-    else:
-        cursor.execute("DELETE FROM content WHERE id = ?", (int(mem[0]),))
-        connection.commit()
-        if mem[2] == "photo":
-            requests.post(url + "/sendPhoto", json={"chat_id": memes_channel_id, "photo": mem[1]})
+
+
+async def main():
+    with closing(sqlite3.connect(database)) as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT id, file_id, file_type FROM content WHERE chat = ? ORDER BY id LIMIT 1",
+                       (chats[memes_chat_id],))
+        mem = cursor.fetchone()
+        if mem is None:
+            reserv_photo = open(os.path.join("reserv/memdelo", random.choice(os.listdir("reserv/memdelo"))), "rb")
+            name_photo = reserv_photo.name
+            await bot.send_photo(memes_channel_id, photo=reserv_photo)
+            reserv_photo.close()
+            os.remove(name_photo)
         else:
-            requests.post(url + "/sendAnimation", json={"chat_id": memes_channel_id, "animation": mem[1]})
+            cursor.execute("DELETE FROM content WHERE id = ?", (int(mem[0]),))
+            connection.commit()
+            if mem[2] == "photo":
+                await bot.send_photo(memes_channel_id, photo=mem[1])
+            else:
+                await bot.send_animation(memes_channel_id, animation=mem[1])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
